68f520677584dfc925c09f254864d1bdef57ba4e
황정현 <jhhwang@wemakeprice.com>
2024-01-14 15:12:57 +0900

    study - java 이론

---
+++ src/class1/ClassStart1.java
@@ -0,0 +1,14 @@
+package class1;
+
+public class ClassStart1 {
+
+    public static void main(String[] args) {
+        String student1 = "학생1";
+        int student1Age = 15;
+        int student1Grade = 90;
+
+        String student2 = "학생2";
+        int student2Age = 16;
+        int student2Grade = 80;
+    }
+}
